9a6b9840cc7331d33310184cc44df3ec49a12fe4
Aleksandr Guljajev <alexandr.gulyayev@gmail.com>
2015-04-29 20:38:09 +0200

    Adding helper method

--- src/dev/core.clj
+++ src/dev/core.clj
@@ -7,3 +7,7 @@
   (slurp (clojure.java.io/file  (clojure.java.io/resource res-file))))
 
 
+(defn parse-f [file-name]
+  (reset! ignore (insta/parser (get-contents "whitespace.bnf")))
+  (reset! grammar (insta/parser (get-contents "phpgrammar.bnf") :auto-whitespace @ignore))
+  (grammar (get-contents file-name)))
