894ea300ce0527631fd8afb95aa0b1b29ba1ca08
Aleksandr Guljajev <alexandr.gulyayev@gmail.com>
2015-05-20 23:36:25 +0200

    tree traversal (wip)

--- src/dev/core.clj
+++ src/dev/core.clj
@@ -4,33 +4,36 @@
             [pacl.protocols :as p]
             [clojure.walk :as walk] :reload-all))
 
-(def fil-cl (rdr/get-ast (io/resource "class.php")))
+(declare walk-tree)
 
 (defmulti processtreepart
   (fn [el]
     (if (coll? el)
-      (first el)
-      el )
+      (first el))))
+
+
+(defn tree-assembler [current-tree element]
+  (let [processed (processtreepart element)
+        tree (if (nil? processed) current-tree (conj current-tree processed))]
+    (fn [] (walk-tree element tree tree-assembler))
     ))
 
-(defn tree-assembler [element]
-  (processtreepart element)
-  (fn [] (walk-tree element tree-assembler)))
+(defn walk-tree
+  ([top processor-fn]
+   (walk-tree top [] processor-fn))
+  ([top tree processor-fn]
+   (let [ only-colls (filter coll? top)  ]
+     (if (empty? only-colls)
+       tree
+       (map (fn [element]
+              (trampoline (fn [el] (processor-fn tree el)) element))
+            only-colls)))))
 
-
-(defn walk-tree [tree processor-fn]
-  (map (fn [element]
-         (trampoline
-          (fn [el]
-            (when (coll? el)
-              (processor-fn el))) element))
-       tree))
-
-(defmethod processtreepart :class_declaration_statement [el] (println "class declaration" el) )
-(defmethod processtreepart :class_statement [el] (println "class statement" :statement))
-(defmethod processtreepart :method_modifiers [el] (println "methodmodifiers") )
-(defmethod processtreepart :property_list [el] (println "property_list" (second el)))
-(defmethod processtreepart :variable_modifiers [el] (println "variable modifiers") )
-(defmethod processtreepart :property [el] (println "property" el))
+(defmethod processtreepart :class_declaration_statement [el]  (nth el 2))
+(defmethod processtreepart :class_statement [el]  :statement)
+(defmethod processtreepart :method_modifiers [el]  :method_modifiers)
+(defmethod processtreepart :property_list [el]  :property_list)
+(defmethod processtreepart :variable_modifiers [el]  :variable_modifiers)
+(defmethod processtreepart :property [el]  :property)
 (defmethod processtreepart :default [el])
 
