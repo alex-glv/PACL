529c2db9c6d476898005872162393c4de19246c6
Aleksandr Guljajev <alexandr.gulyayev@gmail.com>
2015-06-11 22:12:39 +0200

    Fixing tree traversal

--- src/pacl/core.clj
+++ src/pacl/core.clj
@@ -6,6 +6,7 @@
 
 (declare walk-tree replace-last)
 
+
 (defmulti processtreepart
   (fn [el]
     (if (coll? el)
@@ -14,38 +15,45 @@
 
 
 (defn tree-assembler
-  ([ form]
+  ([tree form depth]
    (let [processed (processtreepart form)
-         children (if (coll? form)
-                    (map (fn [el] (walk-tree el #'tree-assembler)) form)
-                    nil)]
-     (if (nil? processed)
-       (if (empty? children)
-         nil
-         children))
-       (if (empty? children)
-         processed
-         [processed [children]]))))
+         inner-tree (if (and (coll? form) (not (empty? form)))
+                      (walk-tree tree  (first form) #'tree-assembler (inc depth))
+                      tree)]
+     (if (not (nil? processed))
+       (let [new-tree (conj tree {:element processed :depth depth})]
+         new-tree)
+       inner-tree)
+     )))
 
-
-(defn walk-tree [top processor-fn]
-  (let [children (map (fn [element]
-                        (let [res (processor-fn element)]
-                          res))
-                      top)
-        non-empty-ch (filter (complement nil?)  children)]
-
-    non-empty-ch))
+(defn walk-tree
+  ([top processor-fn]
+   (walk-tree '() top processor-fn 0))
+  ([tree top processor-fn depth]
+   (let [new-tree (processor-fn tree top depth)]
+     (if (and (coll? top) (not (empty? top)))
+       (walk-tree new-tree (rest top) processor-fn depth)
+       new-tree
+       ))
+   ))
 
 (defn replace-last [coll item]
   (let [coll-last (last coll)]
     (conj (pop coll) [coll-last [item]])))
 
-(defmethod processtreepart :class_declaration_statement [el] :class_declaration_statement)
-(defmethod processtreepart :class_statement [el]  :statement)
-(defmethod processtreepart :method_modifiers [el]  :method_modifiers)
-(defmethod processtreepart :property_list [el]  :property_list)
-(defmethod processtreepart :variable_modifiers [el]  :variable_modifiers)
-(defmethod processtreepart :property [el]  :property)
+(defmethod processtreepart :class_declaration_statement [el] {:class_name  (second (nth el 2))})
+
+(defmethod processtreepart :class_statement [el]
+  (let [type (first (rest el))]
+    (case type
+      :method_modifiers
+      (let [name (nth el 4)]
+        {:class_member_name name})
+      nil))
+  )
+
+(defmethod processtreepart :extends_from [el] {:extends_form (last  (last  (last  (rest (nth el 2)))))})
+(defmethod processtreepart :member_modifier [el]  {:scope (first el) :type (first (second el))})
+(defmethod processtreepart :property [el] {:var_name (last (second el))})
 (defmethod processtreepart :default [el] nil)
 
